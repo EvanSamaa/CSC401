039cb465c67cb67aefa93bd08bc81958cf81ba72
evan pan <evan12345pan@hotmail.com>
2020-02-08 18:09:07 -0500

    g

--- Ass1/code/a1_extractFeatures.py
+++ Ass1/code/a1_extractFeatures.py
@@ -176,10 +176,8 @@ def extract2(feats, comment_class, comment_id):
         function adds feature 30-173). This should be a modified version of 
         the parameter feats.
     '''
-    print(comment_class)
-    print(comment_id)
-    print(LEFT_NPARR.shape)
     if comment_class == "Left":
+        print(LEFT_DICT[comment_id])
         feat_per_comment = LEFT_NPARR[LEFT_DICT[comment_id],:]
     elif comment_class == "Right":
         feat_per_comment = RIGHT_NPARR[RIGHT_DICT[comment_id],:]
