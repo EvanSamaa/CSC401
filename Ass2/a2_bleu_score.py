70bf7244810df66dac1e07d5a635e0c9451b2f07
evan pan <evan12345pan@hotmail.com>
2020-02-29 16:04:46 -0500

    ok

--- Ass2/a2_bleu_score.py
+++ Ass2/a2_bleu_score.py
@@ -25,7 +25,11 @@ def grouper(seq, n):
     -------
     ngrams : list
     '''
-    assert False, "Fill me"
+    list = []
+    for i in range(0, len(seq) - n + 1):
+        list.append(" ".join(seq[i: i + n]))
+    return list
+
 
 
 def n_gram_precision(reference, candidate, n):
@@ -47,7 +51,26 @@ def n_gram_precision(reference, candidate, n):
         The n-gram precision. In the case that the candidate has length 0,
         `p_n` is 0.
     '''
-    assert False, "Fill me"
+
+    # special case
+    if len(candidate) == 0:
+        return 0
+
+    # variable and modified variables
+    reference_n_grams = set(grouper(reference, n))
+    candidate_n_grams = grouper(candidate, n)
+    N = len(candidate_n_grams)
+    C = 0
+
+    # cound occurances
+    for n_gram in candidate_n_grams:
+        if n_gram in reference_n_grams:
+            C = C + 1
+
+    return C/N
+
+
+
 
 
 def brevity_penalty(reference, candidate):
@@ -67,7 +90,20 @@ def brevity_penalty(reference, candidate):
         The brevity penalty. In the case that the candidate transcription is
         of 0 length, `BP` is 0.
     '''
-    assert False, "Fill me"
+    # special case
+    if len(candidate) == 0:
+        return 0
+
+    # variables
+    BP = 0
+    br = len(reference)/len(candidate)
+
+    if br <= 1:
+        BP = 1
+    else:
+        BP = exp(1-br)
+    return BP
+
 
 
 def BLEU_score(reference, hypothesis, n):
@@ -90,4 +126,38 @@ def BLEU_score(reference, hypothesis, n):
     bleu : float
         The BLEU score
     '''
-    assert False, "Fill me"
+    bleu = 1
+
+    for i in range(0+1, n+1):
+        bleu = bleu * n_gram_precision(reference, hypothesis, i)
+    bleu = bleu * brevity_penalty(reference, hypothesis)
+
+    return bleu
+
+
+
+# to test the bleu score
+def test():
+    references = [
+        "It is a guide to action that ensures that the military will forever heed Party commands",
+        "It is the guiding principle which guarantees the military forces always being under command of the Party",
+        "It is the practical guide for the army always to heed the directions of the party"
+    ]
+    candidates = [
+        "It is a guide to action which ensures that the military always obeys the commands of the party",
+        "It is the insure the troops forever hearing the activity guidebook that party direct"
+    ]
+    references_list = []
+    for item in references:
+        references_list.append(item.split())
+        for i in range(0, len(references_list[-1])):
+            references_list[-1][i] = references_list[-1][i].lower()
+    candidates_list = []
+    for item in candidates:
+        candidates_list.append(item.split())
+        for i in range(0, len(candidates_list[-1])):
+            candidates_list[-1][i] = candidates_list[-1][i].lower()
+    n = 2
+    print(BLEU_score(references_list[0], candidates_list[0], n))
+
+# print(grouper(references_list[0], n))
